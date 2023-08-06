84c360b48ffc8d2c63c77bbfb21d8abc27414dfc
UCaromel <caromelulysse@gmail.com>
2023-08-06 22:36:40 +0200

    download telraamStats from correct repo

--- install-packages-deploy.R
+++ install-packages-deploy.R
@@ -1,3 +1,3 @@
 install.packages(c('tidyverse','shiny', 'httr', 'jsonlite', 'lubridate', 'cowplot', 'CPAT', 'synchrony', 'forecast', 'zoo', 'readr', 'mgcv', 'remotes'), repos='https://cran.rstudio.com/')
 remotes::install_version('rsconnect', '0.8.29')
-remotes::install_github('UCaromel/telraamStats')
+remotes::install_github('agistaterre/telraamStats')
